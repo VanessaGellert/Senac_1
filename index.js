f2bf6cb7937fceab08b2ae39d6da87e3351f3acd
Vanessa Gellert <nessagellert@gmail.com>
2021-08-19 00:32:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var numeroA = parseInt(prompt('Digite o número A'))
+var numeroB = parseInt(prompt('Digite o número B'))
+
+var numeroC = numeroA + numeroB
+var numeroD = numeroA - numeroB
+var numeroE = numeroA * numeroB
+var numeroF = numeroA / numeroB
+var numeroG = (2*(numeroA + numeroB))
+
+alert(numeroC)
+alert(numeroD)
+alert(numeroE)
+alert(numeroF)
+alert(numeroG)
